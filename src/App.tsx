c5930fca00a8d9c9ec31d728dfe97951c64fe62d
EvgeniyProkhorov <firerequest@gmail.com>
2021-12-01 20:01:44 +0300

    WOTS_Start

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
+
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <div>
+            <a href="#s">Home</a>
+            <a href="#s">News Feed</a>
+            <a href="#s">Messages</a>
+        </div>
+    );
 }
 
 export default App;
