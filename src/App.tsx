7d3e9370d56cde41a34498a45624abdbbc67d849
EvgeniyProkhorov <firerequest@gmail.com>
2021-12-02 12:47:07 +0300

    WOTS

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+
 
 function App() {
     return (
-        <div>
+        <div className='app-wrapper'>
             <Header/>
-            <Technologies/>
-        </div>
-    );
-}
-
-function Technologies() {
-    return (
-        <div>
-            <ul>
-                <li>css</li>
-                <li>html</li>
-                <li>js</li>
-                <li>react</li>
-            </ul>
-        </div>
-    )
-}
-
-function Header() {
-    return (
-        <div>
-            <a href="#s">Home</a>
-            <a href="#s">News Feed</a>
-            <a href="#s">Messages</a>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
