824b2ee11cd8b18464f6948485fa95dd42c6622d
EvgeniyProkhorov <firerequest@gmail.com>
2022-02-10 19:29:36 +0300

    WOTS-58

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,73 +1,42 @@
+import css from "./users.module.css";
+import userImg from "../../assets/images/User-Profile-PNG-Image.png";
 import React from "react";
-import css from './users.module.css'
-import {UsersPropsType} from "./UsersContainer";
-import axios from "axios";
-import userImg from '../../assets/images/User-Profile-PNG-Image.png'
+import {UsersType} from "../Redux/usersReducer";
 
+type UserFProps = {
+    totalUsersCount: number
+    pageSize: number
+    currentPage: number
+    users: Array<UsersType>
+    follow: (userID: number) => void
+    unfollow: (userID: number) => void
+    onClickPageChanged: (page: number) => void
+}
 
-const Users = ({users, setUsers, followOnClick, unfollowOnClick}: UsersPropsType) => {
-    let getUsers = () => {
-        if (users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response => {
-                    setUsers(response.data.items)
-                })
-            // setUsers([
-            //     {
-            //         id: 1,
-            //         avatar: 'https://source.unsplash.com/random/200x200?sig=incrementingIdentifier',
-            //         fullName: 'Evgen P.',
-            //         location: {
-            //             city: 'SPB',
-            //             country: 'Russia'
-            //         },
-            //         status: 'lorum ipsum',
-            //         followed: true
-            //     },
-            //     {
-            //         id: 2,
-            //         avatar: 'https://source.unsplash.com/random/200x200?sig=incrementingIdentifier',
-            //         fullName: 'Sasha I.',
-            //         location: {
-            //             city: 'Moscow',
-            //             country: 'Russia'
-            //         },
-            //         status: 'ipsum lorum',
-            //         followed: false
-            //
-            //     },
-            //     {
-            //         id: 3,
-            //         avatar: 'https://source.unsplash.com/random/200x200?sig=incrementingIdentifier',
-            //         fullName: 'Anna K.',
-            //         location: {
-            //             city: 'Kiev',
-            //             country: 'Ukraine'
-            //         },
-            //         status: 'LOL',
-            //         followed: true
-            //
-            //     },
-            //     {
-            //         id: 4,
-            //         avatar: 'https://source.unsplash.com/random/200x200?sig=incrementingIdentifier',
-            //         fullName: 'Andrew Y.',
-            //         location: {
-            //             city: 'New-York',
-            //             country: 'USA'
-            //         },
-            //         status: 'ROFL',
-            //         followed: false
-            //
-            //     },
-            // ])
-        }
+ export const Users = (props: UserFProps) => {
+
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+
+    let arrWithPages = []
+    for (let i = 1; i <= pagesCount; i++) {
+        arrWithPages.push(i)
     }
 
     return (
         <div>
-            <button onClick={getUsers}>Get USers</button>
-            {users.map(u => {
+            <div>
+                {arrWithPages.map(p => {
+                    return <span onClick={() => props.onClickPageChanged(p)}
+                                 className={props.currentPage === p ? css.selectedPage : ''}>{p}</span>
+
+                })}
+                {/*<span className={css.selectedPage}>1</span>*/}
+                {/*<span>2</span>*/}
+                {/*<span>3</span>*/}
+                {/*<span>4</span>*/}
+                {/*<span>5</span>*/}
+            </div>
+            {props.users.map(u => {
                 return <div key={u.id}>
                 <span>
                     <div>
@@ -75,8 +44,8 @@ const Users = ({users, setUsers, followOnClick, unfollowOnClick}: UsersPropsType
                     </div>
                     <div>
                         {u.followed
-                            ? <button onClick={() => unfollowOnClick(u.id)}>Unfollow</button>
-                            : <button onClick={() => followOnClick(u.id)}>Follow</button>}
+                            ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
+                            : <button onClick={() => props.follow(u.id)}>Follow</button>}
                     </div>
                 </span>
                     <span>
@@ -94,5 +63,3 @@ const Users = ({users, setUsers, followOnClick, unfollowOnClick}: UsersPropsType
         </div>
     )
 }
-
-export default Users;
---
+++ src/components/Users/UsersOld.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+// import css from './users.module.css'
+// import {UsersPropsType} from "./UsersContainer";
+// import axios from "axios";
+// import userImg from '../../assets/images/User-Profile-PNG-Image.png'
+//
+//
+// const UsersOld = ({users, setUsers, followOnClick, unfollowOnClick}: UsersPropsType) => {
+//     let getUsers = () => {
+//         if (users.length === 0) {
+//             axios.get('https://social-network.samuraijs.com/api/1.0/users')
+//                 .then(response => {
+//                     setUsers(response.data.items)
+//                 })
+//             // setUsers([
+//             //     {
+//             //         id: 1,
+//             //         avatar: 'https://source.unsplash.com/random/200x200?sig=incrementingIdentifier',
+//             //         fullName: 'Evgen P.',
+//             //         location: {
+//             //             city: 'SPB',
+//             //             country: 'Russia'
+//             //         },
+//             //         status: 'lorum ipsum',
+//             //         followed: true
+//             //     },
+//             //     {
+//             //         id: 2,
+//             //         avatar: 'https://source.unsplash.com/random/200x200?sig=incrementingIdentifier',
+//             //         fullName: 'Sasha I.',
+//             //         location: {
+//             //             city: 'Moscow',
+//             //             country: 'Russia'
+//             //         },
+//             //         status: 'ipsum lorum',
+//             //         followed: false
+//             //
+//             //     },
+//             //     {
+//             //         id: 3,
+//             //         avatar: 'https://source.unsplash.com/random/200x200?sig=incrementingIdentifier',
+//             //         fullName: 'Anna K.',
+//             //         location: {
+//             //             city: 'Kiev',
+//             //             country: 'Ukraine'
+//             //         },
+//             //         status: 'LOL',
+//             //         followed: true
+//             //
+//             //     },
+//             //     {
+//             //         id: 4,
+//             //         avatar: 'https://source.unsplash.com/random/200x200?sig=incrementingIdentifier',
+//             //         fullName: 'Andrew Y.',
+//             //         location: {
+//             //             city: 'New-York',
+//             //             country: 'USA'
+//             //         },
+//             //         status: 'ROFL',
+//             //         followed: false
+//             //
+//             //     },
+//             // ])
+//         }
+//     }
+//
+//     return (
+//         <div>
+//             <button onClick={getUsers}>Get USers</button>
+//             {users.map(u => {
+//                 return <div key={u.id}>
+//                 <span>
+//                     <div>
+//                         <img className={css.userPhoto} src={u.photos.small ? u.photos.small : userImg} alt={'pic'}/>
+//                     </div>
+//                     <div>
+//                         {u.followed
+//                             ? <button onClick={() => unfollowOnClick(u.id)}>Unfollow</button>
+//                             : <button onClick={() => followOnClick(u.id)}>Follow</button>}
+//                     </div>
+//                 </span>
+//                     <span>
+//                     <span>
+//                         <div>{u.name}</div>
+//                         <div>{u.status}</div>
+//                     </span>
+//                     <span>
+//                         <div>{"u.location.country"}</div>
+//                         <div>{"u.location.city"}</div>
+//                     </span>
+//                 </span>
+//                 </div>
+//             })}
+//         </div>
+//     )
+// }
+//
+// export default UsersOld;
