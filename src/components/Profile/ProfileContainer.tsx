82d6ff83eeb45fcece07c62f292cde1c5b08064c
EvgeniyProkhorov <firerequest@gmail.com>
2022-03-25 22:49:03 +0300

    compose

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -49,7 +49,6 @@ type OwnProfileProps = MapStateProps & MapDispatchProps
 const mapStateToProps = (state: AppStateType): MapStateProps => {
     return {
         profile: state.profilePage.profile,
-        // isAuth: state.auth.isAuth
     }
 }
 
